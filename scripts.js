59a21953efa3e8f55eb36cf687bed5f411d35c98
BrenodePaiva <brenopaiva552@gmail.com>
2023-07-30 17:08:26 -0300

    Coloando a escolha da Alexa

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@
 const playHuman = (humanChoiser) => {
  console.log(humanChoiser) 
 }
+
+const playMachine = () => {
+    const choise = ["rock", "paper", "scissors"]
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choise[randomNumber]
+}
