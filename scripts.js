a6a6897c700b44651bba15a84a4ccd936380130b
BrenodePaiva <brenopaiva552@gmail.com>
2023-07-31 13:29:52 -0300

    Projeto Finalizado

--- scripts.js
+++ scripts.js
@@ -1,12 +1,50 @@
 
-
-const playHuman = (humanChoiser) => {
- console.log(humanChoiser) 
-}
-
+// A escolha da máquina 
 const playMachine = () => {
-    const choise = ["rock", "paper", "scissors"]
+    const MchocerIcon = document.querySelector("#alexa-choce-icon")
+    const choce = ["rock", "paper", "scissors"]
+    const choceIcon = ["&#9994;", "&#9995;", "&#9996;"]
     const randomNumber = Math.floor(Math.random() * 3)
 
-    return choise[randomNumber]
+    const machinechocer = [choce[randomNumber], choceIcon[randomNumber]]
+
+    MchocerIcon.innerHTML = machinechocer[1] 
+
+    return machinechocer
+}
+
+// Verificando quem ganhou
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playTheGame = (human, machine) => {
+    const result = document.querySelector(".result")
+    const yourScore = document.querySelector("#human")
+    const machineScore = document.querySelector("#machine")
+
+    if (human === machine) {
+        result.innerHTML = "Deu Empate! &#129309;"
+
+    } else if (human === "rock" && machine === "scissors" || 
+              (human === "paper" && machine === "rock") || 
+              (human === "scissors" && machine === "paper")) {
+        
+        result.innerHTML = "Você Ganhou! &#127942;"
+        humanScoreNumber++
+        yourScore.innerHTML = humanScoreNumber
+
+    } else {
+        result.innerHTML = "Você Perdeu! &#128531;"
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+
+    }
+}
+
+// A escolha do Humano
+const playHuman = (humanchocer, humanchocerIcon) => {
+    const HchocerIcon = document.querySelector("#you-choce-icon")
+    HchocerIcon.innerHTML = humanchocerIcon 
+
+    playTheGame(humanchocer, playMachine()[0])
 }
